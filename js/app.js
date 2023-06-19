312f85b282f2f16248569c050695c14868f16359
JSanLazaro <jsanlazaro@hotmail.com>
2023-06-19 18:16:07 +0200

    hidden visible and reset button

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
 let shapes = document.getElementsByClassName("shape");
 
-console.log(shapes);
-console.log(shapes.length);
 for (let element of shapes) {
   console.log("bucle for");
   element.addEventListener("click", (ev) => {
-    console.log(ev.target);
+    ev.target.style['visibility'] = 'hidden';
   });
 }
+
+document.getElementById('btn_reset').addEventListener("click",(ev)=>{
+  let shapes = document.querySelectorAll('.shape');
+  shapes.forEach((element,index, array) => {
+    element.style['visibility'] = 'visible'    
+  });
+})
